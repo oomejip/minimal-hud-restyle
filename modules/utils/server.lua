0ca9d616405ad25ef02e85c4076b8824c60cfffc
MadCap <95227673+ThatMadCap@users.noreply.github.com>
2025-01-09 22:54:52 +0000

    chore(utils): bump version

--- modules/utils/server.lua
+++ modules/utils/server.lua
@@ -3,7 +3,7 @@ local serverUtils = {}
 serverUtils.versionCheck = function(repository)
 	local resource = GetInvokingResource() or GetCurrentResourceName()
 
-	local currentVersion = "v1.0.9"
+	local currentVersion = "v1.1.0"
 
 	if currentVersion then
 		currentVersion = currentVersion:match("%d+%.%d+%.%d+")
